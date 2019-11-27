63db04dfdd1ab1850712d1800f43697822a5ea3d
Richard Ting <richard@Richards-MacBook-Pro.local>
2019-11-27 23:53:45 +0800

    feat(canvas): render data offscreen

--- app/src/main/java/io/richardtin/playcanvas/MainActivity.kt
+++ app/src/main/java/io/richardtin/playcanvas/MainActivity.kt
@@ -1,12 +1,33 @@
 package io.richardtin.playcanvas
 
-import android.os.Bundle
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        canvas.drawCircle(100f, 100f, 50f, Paint())
+
+        val textPaint = Paint().apply {
+            isAntiAlias = true
+            textSize = 32f
+            color = Color.RED
+            style = Paint.Style.STROKE
+        }
+        canvas.drawText("canvas text", 20f, 180f, textPaint)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageBitmap(bitmap)
     }
 }
